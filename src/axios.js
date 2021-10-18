1e7aef175d9a8a47412d5703fbce543995c9e83c
Devraj Singh <devrajsingh@Devrajs-MacBook-Air.local>
2021-10-18 15:39:24 +0530

    [Final Update Of backend]

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // THE API (cloud function) URL
-  baseURL: 'http://localhost:5001/clone-ad707/us-central1/api'
+  baseURL: 'https://us-central1-clone-ad707.cloudfunctions.net/api'
     // "http://localhost:5001/clone-ad707/us-central1/api",
 });
 
